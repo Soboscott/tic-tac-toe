2890788348aa954986603b64cf1565d1885b4401
laurpaik <laurpaik@gmail.com>
2017-01-24 14:51:08 -0500

    Create function to announce a winner

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,19 +20,42 @@ const gameBoard = {
   player: [
     "x",
     "o"
-  ],
-  setBoard: function () {
-    for (let i = 0; i < this.board.length; i++) {
-      for (let j = 0; j < this.player.length; j++) {
-        let square = this.board[i];
-        let assign = square.replaceWith(this.player[j]);
+  ]
+};
 
-        return assign;
-      }
+// const setBoard = function () {
+//   for (let i = 0; i < this.player.length; i++) {
+//         let square = newBoard[i];
+//         newBoard = square(this.player[j]);
+//       }
+//     };
+//   };
+//   return newBoard;
+// };
+
+// const winRow = function () {};
+// const winColumn = function () {};
+// const winDiag = function () {};
+// const winnerIs = function (player) {
+//   return winRow(player) || winColumn(player) || winDiag(player);
+// };
+// const winner = function() {
+//   if (winnerIs('x')) {
+//     return 'x';
+//   } else if (winnerIs('o')) {
+//     return 'o';
+//   }
+//   return null;
+// };
+const announceWinner = function () {
+  let winner;
+  for (let i = 0; i < this.player.length; i++) {
+    if (winner === this.player[i]) {
+      return "The winner is player " + this.player[i] + "!";
     }
   }
 };
 
 module.export = {
-  gameBoard,
+  gameBoard, announceWinner,
 };
