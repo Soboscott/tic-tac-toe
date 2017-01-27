e681515f370399dc59028b14cdab5ed56b7730c4
laurpaik <laurpaik@gmail.com>
2017-01-27 18:57:18 -0500

    create onSuccess, onError, onPostSuccess

---
+++ assets/scripts/games/ui.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const onSuccess = function (data) {
+  if (data.game) {
+    console.log(data.game);
+  } else {
+    console.log(data.games);
+  }
+};
+
+const onError = function (response) {
+  console.error(response);
+};
+
+const onPostSuccess =function(data) {
+  console.log(data);
+};
+
+module.exports = {
+  onSuccess,
+  onError,
+  onPostSuccess,
+};
