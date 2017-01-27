7ecf3aa8d6a00e5e2d4197afc0d1cb9b01d764ec
laurpaik <laurpaik@gmail.com>
2017-01-27 10:21:40 -0500

    Add pseudocode, add blocked out code to test out
    -gonna test these between commits
    -#betterSafeThanSorry

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -6,7 +6,7 @@ let gameBoard = [
     '-', '-', '-',
   ];
 
-let newGameBoard = [
+const newGameBoard = [
     '-', '-', '-',
     '-', '-', '-',
     '-', '-', '-',
@@ -27,12 +27,22 @@ let turn = 0;
 
 const reset = function () {
   // event.target.id = 'reset';
+  //reassigns gameBoard to a blank board
   gameBoard = newGameBoard;
+  //reassigns fullBoard to default
   fullBoard = false;
+  // for (let i = 0; i < gameBoard.length; i++) {
+  //   for (let j = 0; j < player.length; j++) {
+  //     if (gameBoard[i] === player[j]) {
+  //       document.getElementsByClassName("box").innerText = '';
+  //     }
+  //   }
+  // }
   return gameBoard;
 };
 
 const threeInARow = function (player, cellOne, cellTwo, cellThree) {
+  //determines if selected cells are all equal to the same player
   if ((cellOne === player) && (cellTwo === player) && (cellThree === player)) {
 
     return true;
@@ -40,6 +50,7 @@ const threeInARow = function (player, cellOne, cellTwo, cellThree) {
 };
 
 const winRow = function (player) {
+  //sets conditions for winning a row
   if (threeInARow(player, gameBoard[0], gameBoard[1], gameBoard[2]) ||
   threeInARow(player, gameBoard[3], gameBoard[4], gameBoard[5]) ||
   threeInARow(player, gameBoard[6], gameBoard[7], gameBoard[8])) {
@@ -53,6 +64,7 @@ const winRow = function (player) {
 };
 
 const winColumn = function (player) {
+  //sets conditions for winning a column
   if (threeInARow(player, gameBoard[0], gameBoard[3], gameBoard[6]) ||
   threeInARow(player, gameBoard[1], gameBoard[4], gameBoard[7]) ||
   threeInARow(player, gameBoard[2], gameBoard[5], gameBoard[8])) {
@@ -66,6 +78,7 @@ const winColumn = function (player) {
 };
 
 const winDiag = function (player) {
+  //sets conditions for winning diagonally
   if (threeInARow(player, gameBoard[0], gameBoard[4], gameBoard[8]) ||
   threeInARow(player, gameBoard[2], gameBoard[4], gameBoard[6])) {
 
@@ -77,29 +90,6 @@ const winDiag = function (player) {
   }
 };
 
-// const isBoardFull = function () {
-//   for (let i = 0; i < gameBoard.length; i++) {
-//     if (gameBoard.every(spotTaken === true)) {
-//       fullBoard = true;
-//     }
-//   }
-// };
-
-
-const isSpotTaken = function (element) {
-  return element !== '-';
-};
-
-const isBoardFull = function () {
-  if (gameBoard.every(isSpotTaken)) {
-    fullBoard = true;
-  }
-};
-
-const winnerIs = function (player) {
-  return winRow(player) || winColumn(player) || winDiag(player);
-};
-//
 const tieGame = function () {
   //tests if the board is full AND if there's no winner
   if (fullBoard === true && winRow(player) === false && winColumn(player) === false && winDiag(player) === false) {
@@ -110,27 +100,47 @@ const tieGame = function () {
   }
 };
 
+const isSpotTaken = function (element) {
+  //determines if a cell has already been used
+  return element !== '-';
+};
+
+const isBoardFull = function () {
+  //determines if all the cells have been used
+  if (gameBoard.every(isSpotTaken)) {
+    fullBoard = true;
+  }
+};
+
+const winnerIs = function (player) {
+  //determines who the winner is
+  return winRow(player) || winColumn(player) || winDiag(player);
+};
+//
+
 const getWinner = function () {
-  //checks if the board is full
+  //checks if the board is full so we can run tieGame
   isBoardFull();
   //sets rules for the tie game, winner
   if (tieGame()) {
-    //says the game is over
+    //reassigns gameOver as true
     gameOver = true;
-    return 'tie game';
+    return true;
   } else if (winnerIs(player[0])) {
     gameOver = true;
-    return 'x';
+    return true;
   } else if (winnerIs(player[1])) {
     gameOver = true;
-    return 'o';
+    return true;
   }
 
   else {
+    //if nobody has won or tied, keeps gameOver as false
     gameOver = false;
   }
 };
 
+//prints out the board in the console for visual reference
 const printBoard = function() {
   for (let i = 0; i < gameBoard.length; i+=3) {
     console.log(gameBoard[i] + " " + gameBoard[i + 1] + " " + gameBoard[i + 2]);
@@ -138,8 +148,11 @@ const printBoard = function() {
 };
 
 const yourMove = function (event) {
-  console.log(player.symbol);
+  // console.log(player.symbol);
   let index = event.target.id;
+  //for the length of the gameBoard, if gB[i] is not an empty string,
+  //spotTaken is reassigned as true
+  //defines spotTaken
   for (let i = 0; i < gameBoard.length; i++) {
     if (gameBoard[index] !== '-') {
       spotTaken = true;
@@ -148,15 +161,21 @@ const yourMove = function (event) {
     }
   }
 
+  //sets condition for what happens when the spotTaken is true
+  //cannot move forward, must pick a different spot
   if (spotTaken === true && fullBoard === false) {
     console.log('Please pick somewhere else!');
+    //if spotTaken is false, the next person can move
   } else if (spotTaken === false) {
     turn++;
+    //every other turn is x or o
     if (turn%2 === 0) {
       gameBoard[index] = player[0];
     } else {
       gameBoard[index] = player[1];
     }
+    //if the gameBoard[index] is the same as a player
+    //change the text in index.html to incude the player name
     for (let i = 0; i < player.length; i++) {
       if (gameBoard[index] === player[i]) {
         this.innerText = player[i];
@@ -169,15 +188,27 @@ const yourMove = function (event) {
   printBoard();
   getWinner();
   if (getWinner() === true) {
-    // reset();
     printBoard();
+    reset();
+    return gameBoard;
   }
 };
-
+//
+// const isGameOver = function () {
+//   if (gameOver === true){
+//     // $('.box').off('click', yourMove);
+//   }
+// };
+// //
+// const playGame = function () {
+//   yourMove();
+//   isGameOver();
+// };
+//
 // printBoard();
 // reset();
 // return gameBoard;
-
+//
 
 module.exports = {
   gameBoard,
@@ -191,4 +222,5 @@ module.exports = {
   reset,
   fullBoard,
   printBoard,
+  // playGame,
 };
