126ed6907d13ab7a7f03c80d9a06fa6727eb75bf
laurpaik <laurpaik@gmail.com>
2017-01-26 19:12:40 -0500

    fix game.yourMove so it shows up on jQuery
    -also block out functions still in testing mode

--- assets/scripts/game-events.js
+++ assets/scripts/game-events.js
@@ -1,19 +1,39 @@
 'use strict';
 const game = require('./example');
 
+// const assignBox = function () {
+//   for (let i = 0; i < game.gameBoard.length; i++) {
+//     let box = document.getElementsByClassName('box');
+//     box = box.setAttribute('data-box', game.gameBoard[i]);
+//     for (let j = 0; j < game.player.length; j++) {
+//       box.setAttribute('data-box', game.player[j]);
+//     }
+//   }
+// };
+//
+// const displaySymbol = function () {
+//   if (this.getAttribute('data-box') === 'x') {
+//     return 'x';
+//   } else {
+//     return 'o';
+//   }
+// };
 
 const addHandlers = () => {
   $('#start').on('click', game.reset);
-  $('#boxZero').on('click', game.yourMove(0));
-  $('#boxOne').on('click', game.yourMove(1));
-  $('#boxTwo').on('click', game.yourMove(2));
-  $('#boxThree').on('click', game.yourMove(3));
-  $('#boxFour').on('click', game.yourMove(4));
-  $('#boxFive').on('click', game.yourMove(5));
-  $('#boxSix').on('click', game.yourMove(6));
-  $('#boxSeven').on('click', game.yourMove(7));
-  $('#boxEight').on('click', game.yourMove(8));
+  // $(".box").on('click', assignBox);
+  // $(".box").on('click', displaySymbol);
+  $('#boxZero').on('click', game.yourMove);
+  $('#boxOne').on('click', game.yourMove);
+  $('#boxTwo').on('click', game.yourMove);
+  $('#boxThree').on('click', game.yourMove);
+  $('#boxFour').on('click', game.yourMove);
+  $('#boxFive').on('click', game.yourMove);
+  $('#boxSix').on('click', game.yourMove);
+  $('#boxSeven').on('click', game.yourMove);
+  $('#boxEight').on('click', game.yourMove);
 };
+
 
 module.exports = {
   addHandlers,
