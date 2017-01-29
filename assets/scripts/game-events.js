3108228ecd29bfb06db338c9835c16a61a3047f6
laurpaik <laurpaik@gmail.com>
2017-01-29 14:37:00 -0500

    Add events for clicks on signUp/In/Out/newGame
    -screen changes!

--- assets/scripts/game-events.js
+++ assets/scripts/game-events.js
@@ -1,8 +1,13 @@
 'use strict';
 const game = require('./example');
 
+// const authEvents = require('./auth/events');
+// const authUi = require('./auth/ui');
+
 const addHandlers = () => {
-  $('#reset').on('click', game.reset);
+
+  //click handlers for boxes and resets
+  $('.newGame').on('click', game.reset);
   $('#0').on('click', game.yourMove);
   $('#1').on('click', game.yourMove);
   $('#2').on('click', game.yourMove);
@@ -12,6 +17,41 @@ const addHandlers = () => {
   $('#6').on('click', game.yourMove);
   $('#7').on('click', game.yourMove);
   $('#8').on('click', game.yourMove);
+
+  //initial screen
+  $('#sign-out').hide();
+  $('#change-password').hide();
+  $('#board').hide();
+  $('.banner').hide();
+  $('#game-search').hide();
+  $('#reset').hide();
+  $('#begin').hide();
+
+  //screen changes once someone clicks sign-up, can't sign up again
+  $('#sign-up').on('submit', () => {
+    $('#sign-up').hide();
+  });
+
+  //board doesn't show up until you start a new game
+  $('#begin').on('click', () => {
+    $('#begin').hide();
+    $('#board').show();
+    $('#winner-status').show();
+    $('#reset').show();
+  });
+
+  //on signing out, goes back to original sign in page
+  $('#sign-out').on('submit', () => {
+    $('#sign-in').show();
+    $('#sign-up').show();
+    $('#sign-out').hide();
+    $('#change-password').hide();
+    $('#board').hide();
+    $('.banner').hide();
+    $('#game-search').hide();
+    $('#reset').hide();
+    $('#begin').hide();
+  });
 };
 
 module.exports = {
