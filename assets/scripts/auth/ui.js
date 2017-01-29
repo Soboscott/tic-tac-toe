aed5849a44811a61f1024d355af253ad5ff50d43
laurpaik <laurpaik@gmail.com>
2017-01-29 14:33:47 -0500

    Build signInSuccess and signInFailure
    -on successful sign in, screen changes
    -on failed sign in, button changes to "please try again"

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,14 +1,37 @@
 'use strict';
 
 const success = (data) => {
-  if (data) {console.log(data);}
+  if (data) {
+    console.log(data);
+  }
 };
+
+const signInSuccess = (data) => {
+  if (data) {
+    console.log(data);
+  }
+
+    $('#sign-in').hide();
+    $('#sign-up').hide();
+    $('#change-password').show();
+    $('#sign-out').show();
+    $('#begin').show();
+    $('#game-search').show();
+    $('#search-result').show();
+  };
 
 const failure = (error) => {
   console.error(error);
 };
 
+const signInFailure = (error) => {
+  console.error(error);
+  $('#enter-password').val('please try again.');
+};
+
 module.exports = {
   failure,
   success,
+  signInFailure,
+  signInSuccess,
 };
